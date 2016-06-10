bdd0bf43cb951d0a29f2cd929f1da734f46d8b82
sjone4 <sjones4@sapient.com>
2016-06-09 20:48:12 -0400

    Updated header.jsx and header.scss
    Finally got it looking the way I want it. Keeping it simple for now.

--- src/app/modules/header/header.jsx
+++ src/app/modules/header/header.jsx
@@ -9,15 +9,11 @@ class Header extends React.Component {
     render() {
         return (
             <section className='header row'>
-                <div className='col-sm-12 col-md-4 col-lg-4'>
-                    <h1 className='header__title'>Hi, I'm Scott Jones.</h1>
+                <div className='header__title col-sm-12 col-md-4 col-lg-4 col-md-offset-2 col-lg-offset-2'>
+                    <h1>Hi, I'm Scott Jones.</h1>
                 </div>
-                <div className='header__bodycopy col-sm-12 col-md-7 col-lg-7'>
-                    <p className='header__bodycopy__title'>I like long walks on the beach. I am decent at basketball. The ladies say I am nice to look at. I am a people person.</p>
-                </div>
-                <div className='header_scroll'>
-                    <p className='header__scroll'>Scroll down to see my work</p>
-                    <div className='header__scroll__bottom-arrow'></div>
+                <div className='header__bodycopy col-sm-12 col-md-7 col-lg-4'>
+                    <p className='header__bodycopy__title'>I like long walks on the beach. I am decent at basketball. The ladies say I am nice to look at. I am a people person. I also love doing web development and listening to classic rock.</p>
                 </div>
             </section>
         );
