ea5378a0870f20cfaa71989efbb91b256f44c0f3
sjone4 <sjones4@sapient.com>
2016-07-14 19:14:45 -0400

    Updated footer

--- src/app/modules/footer/footer.jsx
+++ src/app/modules/footer/footer.jsx
@@ -7,15 +7,17 @@ class Footer extends React.Component {
 
     render() {
         return (
-            <section className='footer row'>
-                <ul className='nav-list'>
-                    <li><a href='http://twitter.com/sdj2964'>Twitter</a></li>
-                    <li><a href='http://github.com/scottdj92'>GitHub</a></li>
-                    <li><a href='http://twitter.com/sdj2964'>Link</a></li>
-                </ul>
-                <div className='info row'>
-                    <div className='col-sm-3 col-md-3 col-lg-3 col-sm-offset-5 col-md-offset-5 col-lg-offset-5'>
-                        <p>Made with React.js and Webpack.</p>
+            <section className='footer'>
+                <div className='row'>
+                    <ul className='nav-list'>
+                        <li><a href='http://twitter.com/sdj2964'>Twitter</a></li>
+                        <li><a href='http://github.com/scottdj92'>GitHub</a></li>
+                        <li><a href='mailto:scottdj92@gmail.com' target='_top'>Email</a></li>
+                    </ul>
+                    <div className='info row'>
+                        <div className='small-12 medium-6 large-6 small-offset-4 columns'>
+                            <p>Made with React.js, Webpack, and lots of love. &copy; Scott Jones 2016</p>
+                        </div>
                     </div>
                 </div>
             </section>
