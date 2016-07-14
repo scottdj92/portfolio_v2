af92ba0083df588cbde62065214fcacc4f2dd6d5
sjone4 <sjones4@sapient.com>
2016-07-14 19:36:51 -0400

    fix up contact form to prepare for image upload

--- src/app/modules/contact/contact.jsx
+++ src/app/modules/contact/contact.jsx
@@ -9,22 +9,27 @@ class Contact extends React.Component {
         return (
             <section className='contact'>
                 <div className='row'>
-                    <h1 className='contact__title'>Feel free to contact me anytime!</h1>
-                    <form>
-                        <div className='contact__name row'>
-                            <div className='small-12 medium-4 columns'>
-                                <label for='contact-name' className='contact__name__label'>Your Name</label>
-                                <input type='text' name='contact-name' placeholder='Your Name Here'></input>
+                    <div className='small-12 medium-7 large-7 columns'>
+                        <h1 className='contact__title'>Feel free to contact me anytime!</h1>
+                        <form>
+                            <div className='contact__name row'>
+                                <div className='small-12 medium-4 columns'>
+                                    <label for='contact-name' className='contact__name__label'>Your Name</label>
+                                    <input type='text' name='contact-name' placeholder='Your Name Here'></input>
+                                </div>
                             </div>
-                        </div>
-                        <div className='contact__message row'>
-                            <div className='small-12 medium-7 columns'>
-                                <label for='comment'>Your Comments</label>
-                                <textarea name='comment' placeholder='Contact Me'></textarea>
+                            <div className='contact__message row'>
+                                <div className='small-12 medium-7 columns'>
+                                    <label for='comment'>Your Comments</label>
+                                    <textarea name='comment' placeholder='Contact Me'></textarea>
+                                </div>
                             </div>
-                        </div>
-                        <input className='contact__submit' type='button' type='submit' value='submit'></input>
-                    </form>
+                            <input className='contact__submit' type='button' type='submit' value='submit'></input>
+                        </form>
+                    </div>
+                    <div className='small-12 medium-4 large-4 columns end'>
+                        <img src='http://placehold.it/300'></img>
+                    </div>
                 </div>
             </section>
         );
