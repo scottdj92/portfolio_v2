9c0df3528cfa142d8afb40d0afec048f76703e9c
Scott Jones <scottdj92@gmail.com>
2017-05-28 12:52:38 -0400

    Update footer to reflect bulma framework

--- src/app/modules/footer/footer.jsx
+++ src/app/modules/footer/footer.jsx
@@ -8,15 +8,13 @@ class Footer extends React.Component {
     render() {
         return (
             <section className='footer'>
-                <div className='row'>
-                    <ul className='nav-list'>
+                <div className='columns'>
+                    <ul className='nav-list column is-2 is-offset-4'>
                         <li><a href='http://twitter.com/sdj2964'>Twitter</a></li>
                         <li><a href='http://github.com/scottdj92'>GitHub</a></li>
                     </ul>
-                    <div className='info row'>
-                        <div className='small-12 medium-6 large-6 small-offset-4 columns'>
-                            <p>Made with <a className='footer__link' href='https://facebook.github.io/react/'>React.js</a>, <a className='footer__link'  href='https://webpack.github.io/'>Webpack</a>, and lots of love. &copy; Scott Jones 2016</p>
-                        </div>
+                    <div className='info column is-8'>
+                        <p>Made with <a className='footer__link' href='https://facebook.github.io/react/'>React.js</a>, <a className='footer__link'  href='https://webpack.github.io/'>Webpack</a>, and lots of love. &copy; Scott Jones 2016</p>
                     </div>
                 </div>
             </section>
